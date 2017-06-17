3fc426b898ff565a820d5ac076becb9ec738d37f
Ilya Sivakov <ilyasivakov@gmail.com>
2017-06-17 12:36:25 +0300

    Multiprocessing

--- photo2map.py
+++ photo2map.py
@@ -3,32 +3,46 @@ import math
 from PIL import Image
 import time
 import argparse
+import multiprocessing
 
-
-# ----------------------------------------------------
 earth_r = 6371.0
 earth_geos_h = 35786.0
 electrol_lon = 76.0
 electrol_fov = 17.89
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-R", "--radius", help="Planet radius in kilometers (default " + str(earth_r) + ")", type=float, default=earth_r)
-parser.add_argument("-H", "--height", help="Satellite height above surface (default " + str(earth_geos_h) + ")", type=float, default=earth_geos_h)
-parser.add_argument("-slon", "--sat-longitude", help="Satellite longitude degrees (default " + str(electrol_lon) + ")", type=float, default=electrol_lon)
+parser.add_argument("-R", "--radius", help="Planet radius in kilometers (default " + str(earth_r) + ")",
+                    type=float, default=earth_r)
+parser.add_argument("-H", "--height", help="Satellite height above surface (default " + str(earth_geos_h) + ")",
+                    type=float, default=earth_geos_h)
+parser.add_argument("-slon", "--sat-longitude", help="Satellite longitude degrees (default " + str(electrol_lon) + ")",
+                    type=float, default=electrol_lon)
 
-parser.add_argument("-i", "--input", help="Input image filename (default input.jpg)", type=str, default="input.jpg")
-parser.add_argument("-o", "--output", help="Output image filename (default output.png)", type=str, default="output.png")
+parser.add_argument("-i", "--input", help="Input image filename (default input.jpg)",
+                    type=str, default="input.jpg")
+parser.add_argument("-o", "--output", help="Output image filename (default output.png)",
+                    type=str, default="output.png")
 
-parser.add_argument("-w", "--width", help="Output image width (default 1000)", type=int, default=1000)
-parser.add_argument("-di", help="Do not use bilinear interpolation (default False)", default=False, action="store_true")
+parser.add_argument("-w", "--width", help="Output image width (default 1000)",
+                    type=int, default=1000)
+parser.add_argument("-di", help="Do not use bilinear interpolation (default False)",
+                    default=False, action="store_true")
 
-parser.add_argument("-m", "--mode", help="Target, Vizible or Full (experimental!) mode (default t)", type=str, default="t", choices=["t", "v", "f"])
-parser.add_argument("-p", "--projection", help="Projection (default mer)", type=str, default="mer",  choices=["eq", "mer", "mil"])
-parser.add_argument("-z", "--zoom", help="Image zoom factor (default 1.0)", type=float, default=1.0)
-parser.add_argument("-lat", "--latitude", help="Target latitude degrees (default 0.0)", type=float, default=0.0)
-parser.add_argument("-lon", "--longitude", help="Target longitude degrees (default satellite)", type=float, default=electrol_lon)
+parser.add_argument("-m", "--mode", help="Target, Visible or Full (experimental!) mode (default t)",
+                    type=str, default="t", choices=["t", "v", "f"])
+parser.add_argument("-p", "--projection", help="Projection (default mer)",
+                    type=str, default="mer", choices=["eq", "mer", "mil"])
+parser.add_argument("-z", "--zoom", help="Image zoom factor (default 1.0)",
+                    type=float, default=1.0)
+parser.add_argument("-lat", "--latitude", help="Target latitude degrees (default 0.0)",
+                    type=float, default=0.0)
+parser.add_argument("-lon", "--longitude", help="Target longitude degrees (default satellite)",
+                    type=float, default=electrol_lon)
 
-parser.add_argument("-f", "--fov", help="FOV of input image (default " + str(electrol_fov) + ")", type=float, default=electrol_fov)
+parser.add_argument("-f", "--fov", help="FOV of input image (default " + str(electrol_fov) + ")",
+                    type=float, default=electrol_fov)
+parser.add_argument("-j", "--threads", help="Multithreading (default 1)",
+                    type=int, default=1)
 
 args = parser.parse_args()
 
@@ -57,8 +71,9 @@ elif args.mode == "f":
     target_lon = 0.0
 
 fov = math.radians(args.fov)
+threads_num = args.threads
 # ----------------------------------------------------
-# input_filename = "1.jpg"
+# input_filename = "11.jpg"
 # interpolate = False
 
 # Copenhagen
@@ -89,11 +104,11 @@ visible_angle = math.atan(1.0 / ((2.0 * h_to_r + h_to_r ** 2) ** 0.5))
 
 cos_max_lon = math.cos(max_lon)
 
-input_image = Image.open(input_filename)
-input_pixels = input_image.load()
+input_image = Image.open(input_filename, 'r')
 input_size = input_image.size[0]
 input_center = input_size / 2.0
 fov_scaled = input_size / fov
+
 
 def bilinear_interpolation(pixels, x_px, y_px):
     x0 = math.floor(x_px)
@@ -115,6 +130,7 @@ def bilinear_interpolation(pixels, x_px, y_px):
         return int(wa * Ia[c] + wb * Ib[c] + wc * Ic[c] + wd * Id[c])
 
     return weight(0), weight(1), weight(2)
+
 
 def angle_to_pixel(angle):
     return input_center + angle * fov_scaled
@@ -156,65 +172,99 @@ elif pr_str == "mil":
     max_y_angle = miller_projection(max_lon)
     target_y = miller_projection(target_lat)
 
-
 max_x_angle = max_lon
 
 if args.mode == "t":
     max_y_angle = max_lon
 elif args.mode == "f":
-    max_y_angle = math.pi/4.0
+    max_y_angle = math.pi / 4.0
     zoom = 0.5
     pass
 
 output_height = int(round(output_width * max_y_angle / max_lon))
-output_image = Image.new('RGB', (output_width, output_height))
-output_pixels = output_image.load()
 
-x_mul = 2.0 * max_x_angle / output_width / zoom
-y_mul = 2.0 * max_y_angle / output_height / zoom
 
-x_d = max_x_angle / zoom
-y_d = max_y_angle / zoom
-t0 = time.clock()
+def worker(i, output_image_mutex):
+    t0 = time.clock()
 
-for y in range(0, output_height):
+    x_mul = 2.0 * max_x_angle / output_width / zoom
+    y_mul = 2.0 * max_y_angle / output_height / zoom
 
-    t1 = time.clock()
+    x_d = max_x_angle / zoom
+    y_d = max_y_angle / zoom
 
-    if t1 - t0 > 1:
-        t0 = t1
-        print(str(round(float(y) / output_height * 100.0, 1)) + "%")
+    chunk_size = int(math.ceil(float(output_height) / threads_num))
+    chunk_offset = i * chunk_size
+    next_chunk_offset = (i + 1) * chunk_size
 
-    lat = projection_rev(y_mul * float(y) - y_d - target_y)
+    output_image_part = Image.new('RGB', (output_width, chunk_size))
+    output_pixels_part = output_image_part.load()
 
-    dec_z = math.sin(lat)
-    cos_lat = math.cos(lat)
+    input_pixels = input_image.load()
 
-    for x in range(0, output_width):
-        lon = x_mul * float(x) - x_d + target_lon - satellite_lon
+    for y in range(chunk_offset, next_chunk_offset):
 
-        if math.fabs(lon) > max_lon:
-            continue
+        if i == 0:
+            t1 = time.clock()
+            if t1 - t0 > 1:
+                t0 = t1
+                print(str(round(float(y * threads_num) / output_height * 100.0, 1)) + "%")
 
-        dec_x = cos_lat * math.cos(lon)
+        lat = projection_rev(y_mul * float(y) - y_d - target_y)
 
-        if dec_x < cos_max_lon:
-            continue
+        dec_z = math.sin(lat)
+        cos_lat = math.cos(lat)
 
-        dec_y = cos_lat * math.sin(lon)
-        dec_x_v = h_to_r_1 - dec_x
+        for x in range(0, output_width):
+            lon = x_mul * float(x) - x_d + target_lon - satellite_lon
 
-        fi = angle_to_pixel(math.atan2(dec_y, dec_x_v))
-        psi = angle_to_pixel(math.atan2(dec_z, dec_x_v))
+            if math.fabs(lon) > max_lon:
+                continue
 
-        try:
-            if interpolate:
-                output_pixels[x, y] = bilinear_interpolation(input_pixels, fi, psi)
-            else:
-                output_pixels[x, y] = input_pixels[round(fi), round(psi)]
-        except IndexError:
-            pass
+            dec_x = cos_lat * math.cos(lon)
 
-print("Saving output: " + output_filename)
-output_image.save(output_filename)
-print("Image saved")
+            if dec_x < cos_max_lon:
+                continue
+
+            dec_y = cos_lat * math.sin(lon)
+            dec_x_v = h_to_r_1 - dec_x
+
+            fi = angle_to_pixel(math.atan2(dec_y, dec_x_v))
+            psi = angle_to_pixel(math.atan2(dec_z, dec_x_v))
+
+            try:
+                if interpolate:
+                    output_pixels_part[x, y - chunk_offset] = bilinear_interpolation(input_pixels, fi, psi)
+                else:
+                    output_pixels_part[x, y - chunk_offset] = input_pixels[round(fi), round(psi)]
+            except IndexError:
+                pass
+
+    output_image_mutex.acquire()
+    output_image_thread = Image.open(output_filename)
+    output_image_thread.paste(output_image_part, (0, chunk_offset))
+    output_image_thread.save(output_filename)
+    output_image_mutex.release()
+
+
+def main():
+    threads = []
+
+    output_image = Image.new('RGB', (output_width, output_height))
+    output_image.save(output_filename)
+
+    output_image_mutex = multiprocessing.Lock()
+
+    for i in range(threads_num):
+        t = multiprocessing.Process(target=worker, args=(i, output_image_mutex,))
+        threads.append(t)
+        t.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("Image saved")
+
+
+if __name__ == '__main__':
+    main()
